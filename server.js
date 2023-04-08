2ffda07ee7b3acdc79068e0e6753d831007afc5f
paradosso-debug <79239630+paradosso-debug@users.noreply.github.com>
2023-04-08 02:50:47 -0600

    Update Server.js

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-
+import {fileURLToPath} from 'url';
 //CONFIGURE DOTENV
 dotenv.config();
+
+
 
 //DATABASE CONFIG
 connectDB();
 
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //MIDDLEWARE
